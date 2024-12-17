0bf3f58a10330193ea0a2345348e41fa80c309d4
Ricardo <ricardordfc@gmail.com>
2024-12-17 15:29:09 -0300

    first commit

---
+++ src/test/java/br/com/rdfc/test_practice/TestPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rdfc.test_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
